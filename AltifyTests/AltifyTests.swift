ea9d745cceb7e3af056ac7388d23747d6aee7443
Dave Williams <dave@dave.io>
2025-03-26 14:56:28 +0000

    Initial Commit

---
+++ AltifyTests/AltifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  AltifyTests.swift
+//  AltifyTests
+//
+//  Created by Dave Williams on 26/03/2025.
+//
+
+import Testing
+
+struct AltifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
